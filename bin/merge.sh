6a727cf5477720c940b7ed871f72cfa06edc232a
webr3 <nathan@webr3.org>
2011-05-08 10:59:39 +0100

    prefixes and builder

---
+++ bin/merge.sh
@@ -0,0 +1,5 @@
+cd ..
+git submodule init
+git submodule update
+cat rdf-interfaces/rdfi.js additions/misc.js additions/iri.js additions/parsers.js additions/prefixes.js > rdf.js
+java -jar ../yui.jar rdf.js > rdf.min.js
